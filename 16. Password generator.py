e82dbadbd7da8feded984a0e4db9afbed6bb2081
saucypilot <durianirian@gmail.com>
2022-07-05 00:44:30 -0500

    Don't know what to do in the for loop

--- 16. Password generator.py
+++ 16. Password generator.py
@@ -1,6 +1,18 @@
 import secrets
 
+#Variables I will need
 password = ""
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+symbols = "!#@$%&()[{]}"
+numbers = "01234556789"
+
+#Prompt the user for the passwored length
 length = input("How many characters do you want in your password? ")
 if length < 8:
-    print("Your password is too short, it must be 8 or more. Try again:\n")
+    while True:
+        length = input("Your password is too short, it must be 8 or more. Try again:\n")
+        if length >= 8:
+            break
+
+for 
