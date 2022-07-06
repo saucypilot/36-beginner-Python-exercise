d0ed7103fc8e554a9ed4a81c05c02bf5dfff32e2
saucypilot <durianirian@gmail.com>
2022-07-06 01:30:02 -0500

    Update 16. Password generator.py

--- 16. Password generator.py
+++ 16. Password generator.py
@@ -1,18 +1,19 @@
 import secrets
 
 #Variables I will need
-password = ""
 lowercase = "abcdefghijklmnopqrstuvwxyz"
 uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 symbols = "!#@$%&()[{]}"
 numbers = "01234556789"
 
 #Prompt the user for the passwored length
-length = input("How many characters do you want in your password? ")
+length = int(input("How many characters do you want in your password? "))
 if length < 8:
     while True:
         length = input("Your password is too short, it must be 8 or more. Try again:\n")
         if length >= 8:
             break
 
-for 
+conjoindVariables = lowercase + uppercase + symbols + numbers;
+password = "".join(secrets.choice(conjoindVariables, length))
+print(password)
